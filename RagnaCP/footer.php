b870a2f71f2c1ce18ababfcf9b8fa41d2b85ea96
Marcos Gonçalves <marcos@visie.com.br>
2018-08-19 11:24:07 -0300

    Copyrights dinamico

--- RagnaCP/footer.php
+++ RagnaCP/footer.php
@@ -1,6 +1,6 @@
 	</div>
 	<footer id="colophon" class="site-footer" role="contentinfo">
-		<p><?php require "includes/config.php"; echo $seu_nome; ?> © copyright 2015 Todos os direitos reservados Tema por <a href="https://github.com/jester-frost/RagnaCP/" target="_blank">Marcos</a></p>
+		<p><?php require "includes/config.php"; echo $seu_nome; ?> © copyright <?php echo date('Y'); ?> Todos os direitos reservados Tema por <a href="https://github.com/jester-frost/RagnaCP/" target="_blank">Marcos</a></p>
 	</footer>
 	<div class="behavior"></div>
 	<div class="span-left"></div>

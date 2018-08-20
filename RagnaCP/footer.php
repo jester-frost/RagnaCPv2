45778cd78b18f727440d10b1664112fb824e1399
Marcos Gonçalves <marcos@visie.com.br>
2018-08-20 08:15:38 -0300

    Link para projeto no github

--- RagnaCP/footer.php
+++ RagnaCP/footer.php
@@ -1,6 +1,6 @@
 	</div>
 	<footer id="colophon" class="site-footer" role="contentinfo">
-		<p><?php require "includes/config.php"; echo $seu_nome; ?> © copyright <?php echo date('Y'); ?> Todos os direitos reservados Tema por <a href="https://github.com/jester-frost/RagnaCP/" target="_blank">Marcos</a></p>
+		<p><?php require "includes/config.php"; echo $seu_nome; ?> © copyright <?php echo date('Y'); ?> Todos os direitos reservados Tema por <a href="https://github.com/jester-frost/RagnaCPv2" target="_blank">Marcos</a></p>
 	</footer>
 	<div class="behavior"></div>
 	<div class="span-left"></div>
